ed2ce0546cd577653ffcbcb5f7cf347dbb559aba
Bob Hiltner <bobhilt@gmail.com>
2018-05-10 08:02:40 -1000

    richer guess feedback. track guesses. Change col index on set_answer to match user input.

--- TelePythy.py
+++ TelePythy.py
@@ -22,7 +22,8 @@
 """
 
 from enum import Enum
-CELL_STATE = Enum('CELL_STATE', 'No Yes Unknown Correct')
+
+CELL_STATE = Enum('CELL_STATE', 'Eliminated Retained Unknown Correct')
 
 # ToDo: Refactor opportunity- any advantage to making colors and shaps some sort of Enums as well?
 
@@ -77,45 +78,87 @@ class Board:
     
     def __init__(self):
         self._grid = [[[] for y in range(self._board_size)] for  x in range(self._board_size)]
+        self.eliminated =  {'rows': [], 'cols': [], 'colors' : [], 'shapes': []}
+        self.retained =  {'rows': [], 'cols': [], 'colors' : [], 'shapes': []}
     
     def size(self):
         return self._board_size
     
+    def update_cell_statuses(self,cell,answer_true):
+        if not answer_true:
+            prior_eliminated = len([cell for cell in self.cells 
+                                   if cell.state == CELL_STATE.Eliminated])
+            # ToDo: test this for color, then follow for other attributes.
+            if cell.color not in self.eliminated['colors']:
+                # add to eliminated and set all relevant cell states to 
+                if cell.color in self.retained['colors']:
+                    self.retained['colors'].remove(cell.color)
+                self.eliminated['colors'].append(cell.color)
+                for c in self.cells:
+                    if c.color == cell.color:
+                        c.state = CELL_STATE.Eliminated
+                        
+            post_eliminated = len([cell for cell in self.cells if cell.state == CELL_STATE.Eliminated])
+        
+            return str('Eliminated ' + str(post_eliminated - prior_eliminated) + ' additional cells for a total of ' + str(post_eliminated))
+        
+        else: #add non-eliminated things to retained.
+            if cell.color not in self.eliminated['colors']:
+                if cell.color not in self.retained['colors']:
+                    self.retained['colors'].append(cell.color)
+                for c in self.cells:
+                    if (c.color == cell.color) and (c.state != CELL_STATE.Eliminated):
+                        c.state = CELL_STATE.Retained
 
 class Game:
 
     def __init__(self):
         self.board = Board()
         self._answer = None
+        self._guesses = list()
         
     def _set_answer(self, row, col):
         if self._answer is not None:
             self._answer.state = CELL_STATE.Unknown
-        ndx = (ord(row) - ord('A')) * self.board.size() + col
+        ndx = (ord(row) - ord('A')) * self.board.size() + (col - 1) # board columns are 1-based
         self._answer = self.board.cells[ndx]
         self.board.cells[ndx].state = CELL_STATE.Correct
 
-
-               
-    def guess(self, row, col, color, shape):
+                        
+    def guess(self, row, col, color, shape, try_solve = False):
         if self._answer is None:
             # what to do here? Assign a random cell?
             raise AttributeError('Cannot guess until answer is set')
+        ndx = (ord(row) - ord('A')) * self.board.size() + col-1
+        cell = self.board.cells[ndx]
+        if (cell.color != color) or (cell.shape != shape):
+            raise ValueError('Error in cell values. Did you mean ' + cell.color + ' ' + cell.shape + '?')
         a = self._answer # just to save typing
         # board  col is 1-based
-        return  (row == a.row) or \
-                ((col - 1) == a.col) or \
-                (color == a.color ) or \
-                (shape == a.shape )
-        # ToDo: return info that 
+        self._guesses.append(cell)
+        if try_solve:
+            return a is cell
+        
+        
+        answer_true =  ((row == a.row) or 
+                        ((col - 1) == a.col) or 
+                        (color == a.color ) or 
+                        (shape == a.shape )
+                        )
+        results = self.board.update_cell_statuses(cell,answer_true)
+        
+        return answer_true, results
+        # ToDo: return richer results
+    def guesses(self):
+        return self._guesses
         
 import unittest
 
 class TestGameStartup(unittest.TestCase):
 
     def test_cell_state_descriptions_are_enumerated(self):
-        self.assertEqual(CELL_STATE.No.value, 1)
-        self.assertEqual(CELL_STATE.Yes.value, 2)
+        self.assertEqual(CELL_STATE.Eliminated.value, 1)
+        self.assertEqual(CELL_STATE.Retained.value, 2)
         self.assertEqual(CELL_STATE.Unknown.value, 3)
         self.assertEqual(CELL_STATE.Correct.value, 4)
 
@@ -161,27 +204,47 @@ class TestGameStartup(unittest.TestCase):
         
     def test_set_answer_is_retreivable(self):
         game = Game()
-        game._set_answer('A',17) # A18 pink heart
+        game._set_answer('A',18) # A18 pink heart
         self.assertEqual(game._answer.col,17)
         self.assertEqual(game._answer.shape,game.board.shapes['heart'])
         
 class TestGamePlay(unittest.TestCase):
     def test_guess_with_matching_attributes_returns_true(self):
         game = Game()
-        game._set_answer('A',17) # A18 pink heart
-        self.assertEqual(game.guess('A',1,'blue','bolt'),True) # A
-        self.assertEqual(game.guess('D',18,'blue','circle'),True) # 18
-        self.assertEqual(game.guess('B',12,'pink','star'),True) # pink
-        self.assertEqual(game.guess('Q',8,'green','heart'),True) # heart
-        self.assertEqual(game.guess('R',6,'pink','heart'),True)   # another pink heart
+        game._set_answer('A',18) # A18 pink heart
+        self.assertEqual(game.guess('A',1,'blue','bolt')[0],True) # A
+        self.assertEqual(game.guess('D',18,'blue','circle')[0],True) # 18
+        self.assertEqual(game.guess('B',12,'pink','star')[0],True) # pink
+        self.assertEqual(game.guess('Q',8,'green','heart')[0],True) # heart
+        self.assertEqual(game.guess('R',6,'pink','heart')[0],True)   # another pink heart
 
     def test_guess_with_no_matching_attributes_returns_false(self):
         game = Game()
-        game._set_answer('A',17) # A18 pink heart
-        self.assertEqual(game.guess('K',3,'green','moon'),False)
-        self.assertEqual(game.guess('I',13,'orange','sun'),False)
-        self.assertEqual(game.guess('R',17,'purple','diamond'),False)
-
+        game._set_answer('A',18) # A18 pink heart
+        self.assertEqual(game.guess('K',3,'green','moon')[0],False)
+        self.assertEqual(game.guess('I',13,'orange','sun')[0],False)
+        self.assertEqual(game.guess('R',17,'purple','diamond')[0],False)
+        
+    def test_guess_with_inconsistent_values_rejected(self):
+        game = Game()
+        game._set_answer('A',18) # A18 pink heart
+        with self.assertRaises(ValueError):
+            game.guess('K',3,'green','star') # s/b green moon
+        
+        
+    def test_tracks_each_guess_in_order(self):
+        game = Game()
+        game._set_answer('R',18) # R18 red sun
+        game.guess('A',1,'blue','bolt')
+        game.guess('B',12,'pink','star')
+        self.assertEqual(len(game.guesses()),2)
+        self.assertEqual(game.guesses()[1].color, game.board.colors['pink'])
+        
+    def try_solve_compares_guess_to_set_answer(self):
+        game = Game()
+        game._set_answer('R',18) # R18 red sun
+        self.assertTrue(game.guess('R',18,'red','sun')[0])        
+        self.assertFalse(game.guess('R',6,'pink','heart')[0])
         
 if __name__ == '__main__':
     unittest.main()
